d27569cabca97ac5b002b0e377f0aa750ead8d0b
sreejith-marath <sreejithmnambiar2.0@gmail.com>
2024-04-20 02:04:13 +0530

    git commit -m "log and exception"

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="sreejith",
+    author_email="sreejithmnambiar2.0@gmail.com",
+    packages=find_packages()
+)
